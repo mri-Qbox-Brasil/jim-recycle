f8f4284e5bd07e7233ba0e63fb010988413e1bbd
kaJseia <89431075+mur4i@users.noreply.github.com>
2024-05-10 20:09:57 -0300

    Update pt-br e fixes para base mri_Qbox

--- config.lua
+++ config.lua
@@ -2,14 +2,14 @@ print("^2Jim^7-^2Recycle ^7v^42^7.^47^7.^42 ^7- ^2Recycling Script by ^1Jimathy^
 
 Config = {
 	Debug = false, -- Toggle Debug Mode
-	Lan = "en",
+	Lan = "pt",
 
-	img = "qb-inventory/html/images/", -- Set this to your inventory
+	img = "ox_inventory/web/images/", -- Set this to your inventory
 
-	Inv = "qb", -- set to "ox" if using OX Inventory
-	Menu = "qb",
-	ProgressBar = "qb",
-	Notify = "qb",
+	Inv = "ox", -- set to "ox" if using OX Inventory
+	Menu = "ox",
+	ProgressBar = "ox",
+	Notify = "ox",
 
 	Overrides = {
 		ScrapyardSeraching = true,
@@ -20,7 +20,7 @@ Config = {
 	ScrapyardSearching = {
 		Enable = true,
 		searched = { }, -- No Touch
-		skillcheck = "qb-skillbar", --"qb-lock", "ps-ui", "qb-skillbar", "ox_lib", nil
+		skillcheck = "ox_lib", --"qb-lock", "ps-ui", "qb-skillbar", "ox_lib", nil
 		models = { -- The mighty list of dumpters/trash cans
 			`prop_wreckedcart`, `prop_snow_rub_trukwreck_2`, `prop_wrecked_buzzard`, `prop_rub_buswreck_01`, `prop_rub_buswreck_03`, `prop_rub_buswreck_06`, `prop_rub_carwreck_10`,
 			`prop_rub_carwreck_11`, `prop_rub_carwreck_12`, `prop_rub_carwreck_13`, `prop_rub_carwreck_14`, `prop_rub_carwreck_15`, `prop_rub_carwreck_16`, `prop_rub_carwreck_17`,
@@ -33,7 +33,7 @@ Config = {
 	DumpsterDiving = {
 		Enable = true,
 		searched = { }, -- No Touch
-		skillcheck = "qb-skillbar", --"qb-lock", "ps-ui", "qb-skillbar", "ox_lib", nil
+		skillcheck = "ox_lib", --"qb-lock", "ps-ui", "qb-skillbar", "ox_lib", nil
 		models = { -- The mighty list of dumpters/trash cans
 			`prop_dumpster_01a`, `prop_dumpster_02a`, `prop_dumpster_02b`, `prop_dumpster_3a`, `prop_dumpster_4a`, `prop_dumpster_4b`,
 			`prop_bin_05a`, `prop_bin_06a`, `prop_bin_07a`, `prop_bin_07b`, `prop_bin_07c`, `prop_bin_07d`, `prop_bin_08a`, `prop_bin_08open`,
